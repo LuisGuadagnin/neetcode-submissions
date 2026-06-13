32b94e94e38226886342346536e936f67708ee9b
Luís Guadagnin <luisfguadagnin@gmail.com>
2026-06-13 07:04:47 -0300

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            int frequency = frequencyMap.getOrDefault(num, 0);
+            frequency ++;
+            frequencyMap.put(num, frequency);
+        }
+        List<List<Integer>> buckets = new ArrayList<>();
+        buckets.add(new ArrayList<>());
+        for (int num : nums) {
+            buckets.add(new ArrayList<>());
+        }
+        for (int num : frequencyMap.keySet()) {
+            buckets.get(frequencyMap.get(num)).add(num);
+        }
+
+        int[] result = new int[k];
+        int resultIt = 0;
+        for (int i = buckets.size() - 1; i>=0; i--) {
+            for (int num : buckets.get(i)) {
+                result[resultIt++] = num;
+                if (resultIt == k) return result;
+            }
+        }
+        return result;
+    }
+
+    public int[] topKFrequent2(int[] nums, int k) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        for (int num : nums) {
+            int frequency = frequencyMap.getOrDefault(num, 0);
+            frequency ++;
+            frequencyMap.put(num, frequency);
+        }
+        List<Integer> sortedByFrequency = frequencyMap.entrySet().stream().sorted((entry1, entry2) -> {
+            return entry2.getValue().compareTo(entry1.getValue());
+        }).map(entry -> entry.getKey()).toList();
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = sortedByFrequency.get(i);
+        }
+        return result;
+    }
+}

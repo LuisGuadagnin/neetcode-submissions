2b46a5445af0c86877863678a7b1fa4573b27ae9
Luís Guadagnin <luisfguadagnin@gmail.com>
2026-06-15 14:26:11 -0300

    Add: climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/climbing-stairs/submission-2.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int climbStairs(int n) {
+        Integer[] results = new Integer[n+1];
+        results[0] = 1;
+        return climbStairs(n, results);
+    }
+
+    public int climbStairs(int n, Integer[] results) {
+        if (n == -1) return 0;
+        Integer result = results[n];
+        if (result == null) {
+            result = climbStairs(n - 1, results) + climbStairs(n - 2, results);
+            results[n] = result;
+        }
+        return result;
+    }
+}

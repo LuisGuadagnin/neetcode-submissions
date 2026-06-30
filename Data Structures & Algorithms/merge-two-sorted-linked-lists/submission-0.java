f1a5147861442e0ead221985832820b6468ee5b8
Luís Guadagnin <luisfguadagnin@gmail.com>
2026-06-30 01:01:00 -0300

    Add: merge-two-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-two-sorted-linked-lists/submission-0.java
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode mergedListHead = null;
+        ListNode mergedListLastNode = null;
+
+        while (list1 != null && list2 != null) {
+            int valList1 = list1.val;
+            int valList2 = list2.val;
+
+            if (valList1 < valList2) {
+                if (mergedListHead == null) {
+                    mergedListHead = list1;
+                    mergedListLastNode = list1;
+                } else {
+                    mergedListLastNode.next = list1;
+                    mergedListLastNode = list1;
+                }
+                list1 = list1.next;
+                mergedListLastNode.next = null;
+            } else {
+                if (mergedListHead == null) {
+                    mergedListHead = list2;
+                    mergedListLastNode = list2;
+                } else {
+                    mergedListLastNode.next = list2;
+                    mergedListLastNode = list2;
+                }
+                list2 = list2.next;
+                mergedListLastNode.next = null;
+            }
+        }
+
+        if (list1 == null) {
+            if (mergedListHead == null) {
+                mergedListHead = list2;
+            } else {
+                mergedListLastNode.next = list2;
+            }
+        } else {
+            if (mergedListHead == null) {
+                mergedListHead = list1;
+            } else {
+                mergedListLastNode.next = list1;
+            }
+        }
+
+        return mergedListHead;
+    }
+}
